5ee4090f99d4f2a35ae24d741888a61aeb034e37
IcaroScalisse <icaroscalisse@gmail.com>
2024-02-19 10:24:36 -0300

    limpando arquivos e criando output

--- Pyscripts/GMM.py
+++ Pyscripts/GMM.py
@@ -1,9 +1,36 @@
 import numpy as np
 import pandas as pd
+import os
 from sklearn.mixture import GaussianMixture
 from sklearn.model_selection import GridSearchCV
 
-# Iterate through sabe_dbs list
+folder_path = 'SABE/ncopy/plots/'
+
+if not globals().get('sabe_dbs'):
+    files = []
+    for root, dirs, filenames in os.walk(folder_path):
+        for filename in filenames:
+            if filename.endswith('plot_db.txt'):
+                files.append(os.path.join(root, filename))
+
+    file_names = [os.path.splitext(os.path.basename(file))[0] for file in files]
+    sabe_dbs = []
+
+    for file, file_name in zip(files, file_names):
+        try:
+            df = pd.read_csv(file, sep='\t')
+            sabe_dbs.append(df)
+            print(f"File '{file_name}' read successfully.")
+        except FileNotFoundError:
+            print(f"File '{file_name}' not found. Skipping.")
+
+
+# Now, 'sabe_dbs' contains a list of DataFrames for existing files, and 'file_names' contains corresponding file names
+
+output_folder = "output_GMM"
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
 for idx, sabe_db in enumerate(sabe_dbs):
     # Extract the "Ratio" column from sabe_db
     X = sabe_db['Ratio'].values.reshape(-1, 1)  # Assuming 'Ratio' is the column name
@@ -37,9 +64,9 @@ for idx, sabe_db in enumerate(sabe_dbs):
         }
     )
 
-    # Export the results to a CSV file
-    filename = file_names[idx]  # Assumes you have a corresponding filename for each DataFrame
-    output_csv = f"{filename}_GMM_bic.csv"
+    # Export the results to a CSV file in the output folder
+    filename = file_names[idx]
+    output_csv = os.path.join(output_folder, f"{filename}_GMM_bic.csv")
     df.to_csv(output_csv, index=False)
 
     print(f"Results for {filename} exported to {output_csv}")
